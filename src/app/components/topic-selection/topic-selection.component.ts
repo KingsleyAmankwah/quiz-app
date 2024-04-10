d6c42062f4945ca0eb8a3f8508e6aa34c084489c
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-04-10 14:28:00 +0000

    User interface for topic selection component

---
+++ src/app/components/topic-selection/topic-selection.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-topic-selection',
+  standalone: true,
+  imports: [],
+  templateUrl: './topic-selection.component.html',
+  styleUrl: './topic-selection.component.css'
+})
+export class TopicSelectionComponent {
+
+}
