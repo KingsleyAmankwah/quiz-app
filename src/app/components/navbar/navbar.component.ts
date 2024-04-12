80d69666615dea3553f379393889b8f4f5123ab3
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-04-12 18:14:28 +0000

    navbar component

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { Quiz } from '../../interfaces';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
-
+  selectedQuiz: Quiz | null = null;
+  selectQuiz(quiz: Quiz): void {
+    this.selectedQuiz = quiz;
+  }
 }
