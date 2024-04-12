baa1fcf30cf305114bd3a80a82b4ea8cfb166756
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-04-12 18:13:59 +0000

    topic selection component

--- src/app/components/topic-selection/topic-selection.component.ts
+++ src/app/components/topic-selection/topic-selection.component.ts
@@ -1,13 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { QuizService } from '../../services/quiz.service';
+import { Quiz } from '../../interfaces';
 
 @Component({
   selector: 'app-topic-selection',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './topic-selection.component.html',
   styleUrl: './topic-selection.component.css'
 })
 export class TopicSelectionComponent {
+  quizzes: Quiz[] = [];
 
+  constructor(private quizService: QuizService) {}
+
+  ngOnInit(): void {
+    this.quizService.getQuizData().subscribe((data: { quizzes: Quiz[] }) => {
+      this.quizzes = data.quizzes;
+    });
+  }
 }
