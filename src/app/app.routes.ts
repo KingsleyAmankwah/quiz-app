37766a4db52ff648e31cf00e2704c88e4b0b8242
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-04-12 18:13:16 +0000

    routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,7 @@ import { Routes } from '@angular/router';
 import { TopicSelectionComponent } from './components/topic-selection/topic-selection.component';
 import { ResultsComponent } from './components/results/results.component';
 import { QuizComponent } from './components/quiz/quiz.component';
-import { HtmlQuizComponent } from './components/html-quiz/html-quiz.component';
-import { CssQuizComponent } from './components/css-quiz/css-quiz.component';
-import { JavascriptQuizComponent } from './components/javascript-quiz/javascript-quiz.component';
-import { AccessibilityQuizComponent } from './components/accessibility-quiz/accessibility-quiz.component';
+
 
 export const routes: Routes = [
   {
@@ -18,23 +15,5 @@ export const routes: Routes = [
     component: ResultsComponent,
   },
 
-  {
-    path: 'html-quiz',
-    component: HtmlQuizComponent,
-  },
-
-  {
-    path: 'css-quiz',
-    component: CssQuizComponent,
-  },
-
-  {
-    path: 'javascript-quiz',
-    component: JavascriptQuizComponent,
-  },
-
-  {
-    path: 'accessibility-quiz',
-    component: AccessibilityQuizComponent,
-  },
+  { path: 'quiz/:title', component: QuizComponent },
 ];
