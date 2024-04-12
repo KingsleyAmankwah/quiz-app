685ca88e3e51c4f09e68eb207152e9dc22fac094
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-04-12 16:21:35 +0000

    topic selection component

--- src/app/components/topic-selection/topic-selection.component.ts
+++ src/app/components/topic-selection/topic-selection.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-topic-selection',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './topic-selection.component.html',
   styleUrl: './topic-selection.component.css'
 })
