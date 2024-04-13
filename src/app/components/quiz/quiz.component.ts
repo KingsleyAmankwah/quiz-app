f81d53d14367ad3da3c746a734680aefc30f2345
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-04-13 14:51:29 +0000

    quiz component

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Question, Quiz, QuizData } from '../../interfaces';
 import { QuizService } from '../../services/quiz.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -9,53 +9,88 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './quiz.component.html',
-  styleUrl: './quiz.component.css'
+  styleUrl: './quiz.component.css',
 })
 export class QuizComponent {
   quizData: Quiz | undefined;
+  currentTopic: string | null = null;
   currentQuestionIndex = 0;
   currentQuestion: Question | undefined;
-  selectedOptionIndex: number | null = null;
+  selectedOption: number | null = null;
   showAnswer = false;
+  submitClicked = false;
+  score = 0;
 
-
-  constructor(private quizService: QuizService, private route: ActivatedRoute) {}
+  constructor(
+    private quizService: QuizService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   selectOption(index: number): void {
-    this.selectedOptionIndex = index;
+    if (this.submitClicked) return;
+    this.selectedOption = index;
+    this.submitClicked = false;
   }
 
   submit(): void {
-    if (this.selectedOptionIndex === null) {
-      alert('Please select an option');
-    } else {
+    this.submitClicked = true;
+    if (this.selectedOption !== null) {
       this.showAnswer = true;
+    }
+
+    if (
+      this.selectedOption !== null &&
+      this.currentQuestion?.options[this.selectedOption] ===
+        this.currentQuestion?.answer
+    ) {
+      this.score++;
+      this.quizService.setScore(this.score);
     }
   }
 
   nextQuestion(): void {
+    this.submitClicked = false;
     this.showAnswer = false;
-    this.selectedOptionIndex = null;
+    this.selectedOption = null;
     this.currentQuestionIndex++;
 
-    if (this.quizData && this.quizData.questions) {
+    if (this.quizData && this.currentQuestionIndex < this.quizData.questions.length) {
       this.currentQuestion = this.quizData.questions[this.currentQuestionIndex];
+    } else {
+      this.router.navigate(['/results']);
     }
   }
 
   ngOnInit(): void {
     const title = this.route.snapshot.paramMap.get('title');
+    this.quizService.setTopic(title || '');
     this.quizService.getQuizData().subscribe((data: QuizData) => {
       if (title) {
-
         this.quizData = data.quizzes.find((quiz: Quiz) => quiz.title === title);
         console.log(this.quizData);
-        if (this.quizData && this.quizData.questions) {
-          this.currentQuestion = this.quizData.questions[this.currentQuestionIndex];
-        } else {
-          console.error('Quiz data or questions array is undefined');
+        if (this.quizData) {
+          this.quizService.setSelectedQuiz(this.quizData);
+          if (this.quizData.questions) {
+            this.currentQuestion =
+              this.quizData.questions[this.currentQuestionIndex];
+          } else {
+            console.error('Quiz data or questions array is undefined');
+          }
         }
       }
     });
+  }
+
+  get progress(): string {
+    if (this.quizData && this.quizData.questions) {
+      return (
+        ((this.currentQuestionIndex + 1) / this.quizData.questions.length) *
+          100 +
+        '%'
+      );
+    } else {
+      return '0%';
+    }
   }
 }
