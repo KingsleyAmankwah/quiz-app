73bfb25b4e7e984c8f5bd5871647a0f52bf0a6be
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-04-13 14:52:09 +0000

    results component

--- src/app/components/results/results.component.ts
+++ src/app/components/results/results.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { QuizService } from '../../services/quiz.service';
+import { Router } from '@angular/router';
+import { Quiz } from '../../interfaces';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-results',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './results.component.html',
-  styleUrl: './results.component.css'
+  styleUrl: './results.component.css',
 })
 export class ResultsComponent {
+  score = 0;
+  selectedQuiz: Quiz | null = null;
 
+  constructor(private quizService: QuizService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.quizService.score$.subscribe((score) => {
+      this.score = score;
+
+      this.quizService.selectedQuiz$.subscribe((quiz) => {
+        this.selectedQuiz = quiz;
+      });
+    });
+  }
+
+  playAgain(): void {
+    this.quizService.resetScore();
+    this.quizService.currentTopic$.subscribe((topic) => {
+      if (topic) {
+        this.router.navigate(['/quiz', topic]);
+      }
+    });
+  }
 }
