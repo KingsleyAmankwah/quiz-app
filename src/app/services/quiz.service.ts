c7cf3dcae5cd6ee8091ca61c833d93ffce82576d
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-04-13 14:51:09 +0000

    quiz services

--- src/app/services/quiz.service.ts
+++ src/app/services/quiz.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, map } from 'rxjs';
+import { BehaviorSubject, Observable, map } from 'rxjs';
 import { Quiz, QuizData } from '../interfaces';
 
 @Injectable({
@@ -9,6 +9,31 @@ import { Quiz, QuizData } from '../interfaces';
 export class QuizService {
   private quizURL = '/assets/data.json';
   constructor(private http: HttpClient) {}
+
+  private selectedQuizSubject = new BehaviorSubject<Quiz | null>(null);
+  selectedQuiz$ = this.selectedQuizSubject.asObservable();
+
+  private scoreSubject = new BehaviorSubject<number>(0);
+  score$ = this.scoreSubject.asObservable();
+
+  private currentTopic = new BehaviorSubject<string | null>(null);
+  currentTopic$ = this.currentTopic.asObservable();
+
+  setScore(score: number): void {
+    this.scoreSubject.next(score);
+  }
+
+  resetScore(): void {
+    this.scoreSubject.next(0);
+  }
+
+  setTopic(topic: string): void {
+    this.currentTopic.next(topic);
+  }
+
+  setSelectedQuiz(quiz: Quiz): void {
+    this.selectedQuizSubject.next(quiz);
+  }
 
   getQuizData(): Observable<QuizData> {
     return this.http.get<QuizData>(this.quizURL);
